dbce9bff859d1151094989fffe851ee8cf89b9ef
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2017-08-18 14:15:31 -0500

    starting C the hard way book

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int distance = 10^20;
+    printf("You are %d miles away.\n",distance);
+            printf("and now you are lost.\n");
+            printf( "take your time to find\n" );
+            printf( "and you will see.\n" );
+}
